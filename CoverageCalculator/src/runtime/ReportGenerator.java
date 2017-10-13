13d8f21df6e15b0c82127969a7eb048ee2c51a83
R. David Dunphy <rdd.dunphy@gmail.com>
2017-10-13 21:38:25 +0100

    Introduce j2html library for report generating.

--- CoverageCalculator/src/runtime/ReportGenerator.java
+++ CoverageCalculator/src/runtime/ReportGenerator.java
@@ -1,13 +1,25 @@
 package runtime;
 
+import static j2html.TagCreator.*;
+
 public class ReportGenerator {
 
     public void generate() {
         double methodCoverage = CoverageLogger.getInstance().getMethodCoverage();
         System.out.println("Method coverage: " + formatPercentage(methodCoverage));
+        System.out.println(generateHTML(formatPercentage(methodCoverage)));
     }
 
     private String formatPercentage(double d) {
         return String.format("%.1f%%", d * 100);
+    }
+
+    private String generateHTML(String coverage) {
+        return html(
+                body().with(
+                    h1("Method coverage"),
+                    p(coverage)
+                )
+            ).render();
     }
 }
