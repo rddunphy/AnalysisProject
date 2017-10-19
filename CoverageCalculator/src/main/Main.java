c8c5c4d0a4e4a085e03b2923601f84896a96d5ab
R. David Dunphy <raymond.dunphy.2014@uni.strath.ac.uk>
2017-10-19 18:33:24 +0100

    Rename module from ExampleApplication to FolioTracker.

--- CoverageCalculator/src/main/Main.java
+++ CoverageCalculator/src/main/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String sourceProject = "ExampleApplication";
+        String sourceProject = "FolioTracker";
         String generatedProject = "Generated";
         DirectoryCleaner.cleanProject(sourceProject, generatedProject);
         ProjectGenerator pg = new ProjectGenerator(sourceProject, generatedProject);
